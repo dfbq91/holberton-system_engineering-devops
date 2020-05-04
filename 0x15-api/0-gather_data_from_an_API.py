8c967e875fad03948370f730930d3c9714bd52b1
dfbq91 <dfbq91@gmail.com>
2020-05-04 18:02:04 -0500

    fixed .get

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,14 @@
 TODO list progress'''
 
 
-import csv
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
 
+    done_tasks = 0
+    total_tasks = 0
     jsoned_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
                                format(argv[1])).json()
     jsoned_todos = requests.get('https://jsonplaceholder.typicode.com/todos').\
@@ -18,11 +19,18 @@ if __name__ == "__main__":
     # Get name of the employee
     name = jsoned_user.get('name')
     id = jsoned_user.get('id')
-    filename = "{}.csv".format(id)
 
-    # Open and write into .csv file
-    with open(filename, 'w', encode='utf-8') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for tasks in jsoned_todos:
-            if id == tasks.get('userId'):
-                writer.writerow([id, name, tasks.get('completed'), tasks.get('title')])
+    # Get total and done tasks number
+    for tasks in jsoned_todos:
+        if id == tasks.get('userId'):
+            total_tasks += 1
+            if tasks.get('completed') is True:
+                done_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, done_tasks, total_tasks))
+
+    # Completed tasks
+    for tasks in jsoned_todos:
+        if id == tasks.get('userId') and tasks.get('completed') is True:
+            print("\t{}".format(tasks.get('title')))
