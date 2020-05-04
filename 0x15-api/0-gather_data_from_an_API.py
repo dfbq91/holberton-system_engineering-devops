f250fc57004e4d90877e122ea0e256769f73f947
dfbq91 <dfbq91@gmail.com>
2020-05-04 14:48:47 -0500

    0 task and README

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Given an api, returns information about a
+TODO list progress'''
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    done_tasks = 0
+    total_tasks = 0
+    jsoned_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                               format(argv[1])).json()
+    jsoned_todos = requests.get('https://jsonplaceholder.typicode.com/todos').\
+        json()
+
+    # Get name of the employee
+    name = jsoned_user['name']
+    id = jsoned_user['id']
+
+    # Get total and done tasks number
+    for tasks in jsoned_todos:
+        if id == tasks.get('userId'):
+            total_tasks += 1
+            if tasks.get('completed') is True:
+                done_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, done_tasks, total_tasks))
+
+    # Completed tasks
+    for tasks in jsoned_todos:
+        if id == tasks.get('userId') and tasks.get('completed') is True:
+            print("\t{}".format(tasks.get('title')))
