a88322304afc1375a6e939c17997e6b7226acc3d
dfbq91 <dfbq91@gmail.com>
2020-05-07 18:15:51 -0500

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,10 +14,10 @@ def top_ten(subreddit):
         if r_jsoned.get('data').get('children') is None:
             print(None)
         for post_number in range(1, 11):
-            try:
+            if len(r_jsoned.get('data').get('children')) < 10:
+                print(None)
+            else:
                 print(r_jsoned.get('data').get('children')[post_number].
                       get('data').get('title'))
-            except Exception:
-                print(None)
     else:
         print(None)
