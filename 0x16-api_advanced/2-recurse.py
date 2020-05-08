fcebe853119c70e78bf74b57c377eefd72cbf897
dfbq91 <dfbq91@gmail.com>
2020-05-07 20:43:42 -0500

    recursive to get list of all posts

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''Tasks with subreddit api'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    '''Queries the Reddit API and returns all
+    hot posts listed for a given subreddit'''
+
+    # Set url with query string, do request and covert it to json
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
+    headers = {'User-Agent': 'My name is Diego'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    r_jsoned = r.json()
+
+    # if the subreddit exists... otherwise return None
+    if r.status_code == 200:
+
+        # check if the subreddit has posts
+        if r_jsoned.get('data').get('children') is None:
+            return None
+
+        # Set after to get next page with 25 posts and validate it exists
+        after = r_jsoned.get('data').get('after')
+
+        # Get titles of posts in a hot_list iterating in the page and
+        # with recursion traverse all pages in group of the length of each page
+        for post_number in range(0, len(r_jsoned.get('data').get('children'))):
+            hot_list.append(r_jsoned.get('data').get('children')[post_number].
+                      get('data').get('title'))
+
+        if after == None:
+            return hot_list
+
+        recurse(subreddit, hot_list, after)
+
+    else:
+        return None
