82b8b5fbe6d67ee4e4c7ac40dee6d5938621fab0
dfbq91 <dfbq91@gmail.com>
2020-05-28 23:32:39 -0500

    added require instruction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,12 @@
 # Increase ULIMIT for nginx
 
-exec { 'Increase Ulimit for nginx':
+exec { 'Increase_Ulimit':
      command => 'sed -i "s/15/2000/g" /etc/default/nginx',
-     path    => '/bin/',
+     path    => ['/bin/'],
 }
 
-exec { 'restart nginx':
+exec { 'restart_nginx':
      command => 'service nginx restart',
-     path    => ['/usr/bin']
+     path    => ['/usr/bin'],
+     require => Exec['ulimit'],
+}
