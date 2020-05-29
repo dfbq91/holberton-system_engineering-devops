83c46669bd164cf2e2ab6ca00e8cc1755e34b237
dfbq91 <dfbq91@gmail.com>
2020-05-29 10:09:56 -0500

    espiando este man que tal

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Increase ULIMIT for nginx
 
 exec { 'Increase_Ulimit':
-     command => 'sed -i "s/15/2000/g" /etc/default/nginx',
-     path    => ['/bin/'],
+  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  path    => 'shell',
 }
 
 exec { 'restart_nginx':
-     command => 'service nginx restart',
-     path    => ['/usr/bin'],
-     require => Exec['ulimit'],
+  command => 'service nginx restart',
+  path    => 'shell',
+  require => Exec['Increase_Ulimit'],
 }
